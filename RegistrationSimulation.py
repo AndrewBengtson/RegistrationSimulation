0be05bfabefb029aa509ec8fc872614de882c90d
Andrew Bengtson <agbengtson@gmail.com>
2021-10-07 13:29:06 -0400

    basic implementation of student_register

--- RegistrationSimulation.py
+++ RegistrationSimulation.py
@@ -48,6 +48,25 @@ def bucket_register(students,possible_courses):
     for student in students:
         print(student.name,"wanted",student.desired[0].name,"and got",student.registered[0].name if (len(student.registered)>0) else "nothing")
 
+def student_register(students,possible_courses):
+    #we index the list of students first
+    for student in students:
+        #we try to register the student for classes in their order of preference
+        index = 0
+       # print(student.name,"wants",student.desired[0].name," ",student.desired[1].name)
+        registered = False
+        while(index<len(student.desired) and not registered):
+            #if there is a seat available put the student in it
+            if(len(student.desired[index].registered)<student.desired[index].capacity):
+                student.registered.append(student.desired[index])
+                student.desired[index].registered.append(student)
+                registered = True
+                #print(student.name,"got",student.desired[index].name,"as their ",index,"choice")
+            #if there is not a seat available we just keep the loop
+            index+=1
+    #now we look at the students and see how many got what they wanted
+    for student in students:
+        print(student.name,"wanted",student.desired[0].name,"and got",student.registered[0].name if (len(student.registered)>0) else "nothing")
 
 #we create 13 Courses
 bucket_possible_courses = []
@@ -67,10 +86,14 @@ for i in range(0,5):
     students.append(new_student)
     bucket_student = Student(wanted.copy(),"Student"+str(i))
     bucket_students.append(bucket_student)
+#call the bucket registration process
 bucket_register(bucket_students,bucket_possible_courses)
 print("-----")
-for student in students:
-    print(student.name,"wanted",student.desired[0].name,"and got",student.registered[0].name if (len(student.registered)>0) else "nothing")
+#clean out all of the courses
+for course in bucket_possible_courses:
+    course.registered = []
+    course.students_who_want = []
+student_register(students,possible_courses)
 
         
     
